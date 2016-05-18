c8d6cb96f32a0ca9fdaae6a94d0dde9a46120804
srajana <sneha.rajana@gmail.com>
2016-05-18 12:02:33 -0400

    Changes to baseline script

--- baselines/compute-baselines.py
+++ baselines/compute-baselines.py
@@ -1,4 +1,6 @@
 import math
 import rand
 
-input_files = ['../antonyms/wordnet-antonyms.txt', '../antonyms/wordnet-synonyms-antonyms.txt', 'PPDB-Phrases/phrases-2.0-shorter.txt', '../../antonym-list-1.txt', '../../antonym-list-2.txt', '../../antonym-list-3.txt']
+input_files = ['../antonyms/wordnet-antonyms.txt', '../antonyms/wordnet-synonyms-antonyms.txt', 'PPDB-Phrases/phrases-2.0-shorter.txt', '../antonyms/exclusion-antonyms.txt', '../../antonym-list-1.txt', '../../antonym-list-2.txt', '../../antonym-list-3.txt']
+
+
