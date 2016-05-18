2dd20df128eeec31b7d242b5ea00b1f922a51a68
srajana <sneha.rajana@gmail.com>
2016-05-18 14:23:20 -0400

    new baseline files

--- baselines/compute-baselines.py
+++ baselines/compute-baselines.py
@@ -1,25 +1,26 @@
 import math
 import random
 
-input_files = ['../antonyms/wordnet-antonyms.txt', '../antonyms/wordnet-synonyms-antonyms.txt', '../antonyms/exclusion-antonyms.txt', 'PPDB-Phrases/phrases-2.0-shorter.txt', '../../antonym-list-1.txt', '../../antonym-list-2.txt', '../../antonym-list-3.txt']
+input_files = ['../antonyms/wordnet-antonyms.txt', '../antonyms/wordnet-synonyms-antonyms.txt', '../antonyms/exclusion-antonyms.txt', '../antonyms/not-antonyms.txt', '../../antonym-list-1.txt', '../../antonym-list-2.txt', '../../antonym-list-3.txt']
 
 baseline_antonyms = set()
 
 for antfile in input_files:
-	num_lines = int(randrange(50,100))
+	num_lines = int(random.randrange(50,100))
 	print(antfile, num_lines)
 	f = open(antfile, 'r')
-	lines = list(antfile.readlines())
+	lines = list(f.readlines())
 	f.close()
 	random.shuffle(lines)
 	random_lines = lines[:num_lines]
-	for l in random_lines():
+	for l in random_lines:
 		pair = l.strip('\n')
 		pair = pair.split('\t')
 		baseline_antonyms.add((pair[0], pair[1]))
 
 print(len(baseline_antonyms))
 antonyms = list(baseline_antonyms)
+random.shuffle(antonyms)
 
 fout = open('baseline-antonyms.txt', 'w')
 for entry in antonyms:
